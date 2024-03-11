8dce1621d1f8d6ec4f6c06e6283df7b7e5f3b473
Monnapse <masonshuber@gmail.com>
2024-03-11 17:02:48 -0600

    index-money

---
+++ chart-types/time-index/InfiniteTable.js
@@ -0,0 +1,107 @@
+/**
+ * Infinite Tale.
+ * Made by Monnapse
+ * Allows user to add a new value to table
+ */
+document.addEventListener("DOMContentLoaded", event=>{
+    //document.getElementsById("chart").customCallback();
+
+    // PRESET NODES just add to the end ?nodes=1.32,10:00:30,5.34,08:03:03,3.6543,17:53:00
+    // http://127.0.0.1:3000/ExampleSite/time-money/index.html?nodes=1.32,10:00:30,5.34,08:03:03,3.6543,12:53:05,0.3,03:03:03
+    //console.log(document.URL);
+    var url = new URL(document.URL);
+    var nodes = url.searchParams.get("nodes");
+    if (nodes) {
+        var nodesList = nodes.split(",");
+        nodesList.forEach(node=>{
+            var price = nodesList[0];
+            var time = nodesList[1];
+            nodesList.splice(0,2);
+            createNodeInput(price, time);
+        })
+        var price = nodesList[0];
+        var time = nodesList[1];
+        nodesList.splice(0,2);
+        createNodeInput(price, time);
+    } else {
+        createNodeInput(5.35, "06:00:00");
+        createNodeInput(3.41, "14:36:00");
+        createNodeInput(3.01, "18:00:00");
+        createNodeInput(1.01, "12:00:00");
+        createNodeInput(7.01, "08:00:00");
+    }
+
+    document.getElementById("new-value-button").addEventListener("click", event=>{
+        createNodeInput();
+    })
+})
+
+function createNodeInput(value1, value2) {
+    /*
+        <tr>
+            <th><input type="text"></th>
+            <th><input type="time"></th>
+        </tr>
+        */
+        var tr = document.createElement("tr");
+
+        var priceTh = document.createElement("th");
+        tr.appendChild(priceTh);
+        var priceInput = document.createElement("input");
+        priceInput.type = "number";
+        priceInput.step = "0.01";
+        priceInput.min = "0";
+        priceTh.appendChild(priceInput);
+        if (value1) {
+            priceInput.value = value1
+        }
+        
+        var timeTh = document.createElement("th");
+        tr.appendChild(timeTh);
+        var timeInput = document.createElement("input");
+        timeInput.type = "time";
+        timeTh.appendChild(timeInput);
+        if (value2) {
+            timeInput.value = value2;
+        }
+ 
+        var chart = document.getElementById("chart")
+        var nodeElement = document.createElement("value-cgjs"); // <value-cgjs value1="5.35" value2="6:00"></value-cgjs>
+        var parented = false;
+        var price = value1 || undefined;
+        var time = value2 || undefined;
+
+        if (price && time) {
+            nodeElement.setAttribute("value1", price);
+            nodeElement.setAttribute("value2", time);
+            parented = true;
+            chart.appendChild(nodeElement);
+        }
+ 
+        priceInput.addEventListener("change", event=>{
+         //console.log(event.target.value);
+            price = event.target.value;
+            if (time) {
+                nodeElement.setAttribute("value1", price);
+
+                if (!parented) {
+                    parented = true;
+                    chart.appendChild(nodeElement);
+                }
+             }
+        })
+        timeInput.addEventListener("change", event=>{
+         //console.log(event.target.value);
+             time = event.target.value;
+             if (price) {
+                 nodeElement.setAttribute("value2", time);
+ 
+                 if (!parented) {
+                     parented = true;
+                     chart.appendChild(nodeElement);
+                 }
+             }
+        })
+ 
+        document.getElementById("value-list").insertBefore(tr, document.getElementById("new-value-button"))
+}
--- releases/CGJS.js
+++ releases/CGJS.js
@@ -27,6 +27,7 @@ class CGJSElementNames {
 }
 class CGJSChartTypes {
     static timeMoney = "time-money";
+    static indexMoney = "index-money";
 }
 class CGJSNodeValues {
     static id = "id";
@@ -55,7 +56,9 @@ class CGJSChart extends HTMLElement {
 
             if (type == "time-money") {
                 this.chartTypeClass = new CGJSTimeMoney(this);
-            } else {
+            } else if (type == "index-money") {
+                this.chartTypeClass = CGJSIndexMoney(this);
+            } else {12345
                 // No type specified or type misspelled
                 this.chartTypeClass = new CGJSChartType(this);
                 return;
@@ -925,6 +928,472 @@ class CGJSTimeMoney extends CGJSChartType {
         const formattedTime = `${formattedHours}:${formattedMinutes} ${period}`;
         return formattedTime;
     }
+    getNodePointById(nodeId) {
+        var node;
+        this.nodePointArray.forEach(nodePoint=>{
+            if (nodePoint.id == nodeId) {node = nodePoint;}
+        })
+        return node;
+    }
+}
+
+class CGJSIndexMoney extends CGJSChartType {
+    /**
+     * The Time And Money Chart, mostly made to display stock chart.
+     */
+
+    buildChartStyle() {
+        //console.log(this.chartClass);
+
+        // Create a shadow root
+        const shadowRoot = this.chartClass.shadowRoot || this.chartClass.attachShadow({ mode: "open" }); 
+
+        this.chartClass.shadowRoot.innerHTML = `
+        <style>
+            .chart-cgjs-side {
+                display: flex;
+                width: 75px;
+                height: calc(100% - 70px);
+                position: absolute;
+                top: 50%;
+                -moz-transform: translateY(-50%); /* Firefox */
+                -ms-transform: translateY(-50%);  /* IE 9 */
+                -webkit-transform: translateY(-50%); /* Safari and Chrome*/
+                -o-transform: translateY(-50%); /* Opera */
+                transform: translateY(-50%);
+                flex-direction: column;
+                justify-items: center;
+            }
+            .chart-cgjs-side span {
+                position: absolute;
+                text-indent: 15px;
+                font-weight: 100;
+                font-size: 0.75em;
+            }
+            .chart-cgjs-side span:first-child {
+                display: flex;
+                align-items: flex-start;
+            }
+            .chart-cgjs-side span:last-child {
+                display: flex;
+                align-items: flex-end;
+            }
+            .chart-cgjs-bottom {
+                display: flex;
+                width: calc(100% - 150px);
+                height: 35px;
+                position: absolute;
+                left: 50%;
+                bottom: 0px;
+                -moz-transform: translateX(-50%); /* Firefox */
+                -ms-transform: translateX(-50%);  /* IE 9 */
+                -webkit-transform: translateX(-50%); /* Safari and Chrome*/
+                -o-transform: translateX(-50%); /* Opera */
+                transform: translateX(-50%);
+                flex-direction: row;
+                justify-content: space-between;
+                justify-items: center;
+            }
+            .chart-cgjs-bottom span {
+                display: flex;
+                flex: 1;
+                font-weight: 100;
+                font-size: 0.75em;
+                justify-content: center;
+                align-items: center;
+            }
+            .chart-cgjs-bottom span:first-child {
+                display: flex;
+                justify-content: flex-start;
+            }
+            .chart-cgjs-bottom span:last-child {
+                display: flex;
+                justify-content: flex-end;
+            }
+            .chart-cgjs-chart {
+                display: block;
+                width: calc(100% - 150px);
+                height: calc(100% - 70px);
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                -moz-transform: translate(-50%, -50%); /* Firefox */
+                -ms-transform: translate(-50%, -50%);  /* IE 9 */
+                -webkit-transform: translate(-50%, -50%); /* Safari and Chrome*/
+                -o-transform: translate(-50%, -50%); /* Opera */
+                transform: translate(-50%, -50%);
+            }
+            .chart-cgjs-chart-points {
+                display: block;
+                width: calc(100% - 135px);
+                height: calc(100% - 55px);
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                -moz-transform: translate(-50%, -50%); /* Firefox */
+                -ms-transform: translate(-50%, -50%);  /* IE 9 */
+                -webkit-transform: translate(-50%, -50%); /* Safari and Chrome*/
+                -o-transform: translate(-50%, -50%); /* Opera */
+                transform: translate(-50%, -50%);
+            }
+            .cgjs-chart-label {
+                position: absolute;
+                display: block;
+                width: 150px;
+                height: fit-content;
+                z-index: 3;
+                border-radius: 10px;
+                backdrop-filter: blur(2px);
+                padding: 5px 22px;
+            }
+            .cgjs-chart-label-value {
+                display: flex;
+                justify-content: space-between;
+            }
+            .cgjs-chart-label-value span {
+                display: flex;
+                font-size: 0.6em;
+                height: 15px;
+                align-items: center;
+            }
+            .cgjs-chart-label-value span:first-child {
+                font-weight: 200;
+            }
+            .cgjs-chart-label-value span:last-child {
+                font-weight: 400;
+            }
+            .cgjs-chart-value-point-circle {
+                border-radius: 100px;
+                display: block;
+                width: 10px;
+                height: 10px;
+            }
+        </style>
+        `; // Load default style sheet for this element
+
+        let theme = this.chartClass.getAttribute("theme");
+        //let value1 = this.chartClass.getAttribute("value1");
+        //let value2 = this.chartClass.getAttribute("value2");
+        //console.log(type, value1, value2);
+        //ctx.fillStyle = CGJSThemes[theme].inner;
+
+        // Theme
+        const styleSheetOutside = document.createElement("link");
+        styleSheetOutside.rel = "stylesheet";
+        styleSheetOutside.type = "text/css";
+        styleSheetOutside.href = "https://monnapse.github.io/cgjs.github.io/releases/ChartThemes/"+theme+"-outside.css";
+
+        const styleSheetInside = document.createElement("link");
+        styleSheetInside.rel = "stylesheet";
+        styleSheetInside.type = "text/css";
+        styleSheetInside.href = "https://monnapse.github.io/cgjs.github.io/releases/ChartThemes/"+theme+"-inside.css";
+
+        document.head.appendChild(styleSheetOutside);
+        shadowRoot.appendChild(styleSheetInside);
+
+        // Chart Layout
+        this.side = document.createElement("div");
+        this.side.classList.add("chart-cgjs-side");
+
+        this.bottom = document.createElement("div");
+        this.bottom.classList.add("chart-cgjs-bottom");
+
+        this.chartCanvas = document.createElement("canvas");
+        this.chartCanvas.classList.add("chart-cgjs-chart");
+        this.chartCanvas.width = this.chartCanvas.clientWidth;
+        this.chartCanvas.height = this.chartCanvas.clientHeight;
+
+        this.chartCanvasDashed = document.createElement("canvas");
+        this.chartCanvasDashed.classList.add("chart-cgjs-chart");
+        this.chartCanvasDashed.width = this.chartCanvasDashed.clientWidth;
+        this.chartCanvasDashed.height = this.chartCanvasDashed.clientHeight;
+
+        this.chartCanvasPoints = document.createElement("canvas");
+        //this.chartCanvasPoints.classList.add("chart-cgjs-chart");
+        this.chartCanvasPoints.classList.add("chart-cgjs-chart-points");
+        this.chartCanvasPoints.width = this.chartCanvasPoints.clientWidth;
+        this.chartCanvasPoints.height = this.chartCanvasPoints.clientHeight;
+
+        shadowRoot.appendChild(this.side);
+        shadowRoot.appendChild(this.bottom);
+        shadowRoot.appendChild(this.chartCanvas);
+        shadowRoot.appendChild(this.chartCanvasDashed);
+        shadowRoot.appendChild(this.chartCanvasPoints);
+
+        //this.drawLine(0,0,50,50);
+
+        return this.chartCanvas;
+    }
+
+    setVerticalValues(values) {
+        // Clear the side
+        this.side.innerHTML = "";
+
+        const lowestValue = this.chartClass.getLowestNodeValue(CGJSNodeValues.actualFirstValue);
+        const highestValue = this.chartClass.getHighestNodeValue(CGJSNodeValues.actualFirstValue);
+
+        const lowestCost = document.createElement("span");
+        lowestCost.textContent = "$"+lowestValue;
+        lowestCost.classList.add("cgjs-vertical-value");
+        lowestCost.style.bottom = "0";
+
+        const highestCost = document.createElement("span");
+        highestCost.textContent = "$"+highestValue;
+        highestCost.classList.add("cgjs-vertical-value");
+        highestCost.style.top = "0";
+
+        var average = 0;
+        values.forEach(value=>{
+            average += parseFloat(value);
+        })
+        average = average/values.length;
+        const averageCost = document.createElement("span");
+        averageCost.textContent = "$"+average.toFixed(2);
+        averageCost.classList.add("cgjs-vertical-value");
+        //console.log(average, lowestValue, highestValue);
+        averageCost.style.bottom = getPercentage(average, lowestValue, highestValue)*100+"%";
+
+        const reversedAveragePercent = getReversedPercentage(average, lowestValue, highestValue)-0.03;
+        this.chartClass.chartTypeClass.drawLine(undefined, 0, reversedAveragePercent, 1, reversedAveragePercent, true);
+
+        this.side.appendChild(highestCost);
+        this.side.appendChild(averageCost);
+        this.side.appendChild(lowestCost);
+    }
+
+    setHorizontalValues(values) {
+        // Clear the bottom
+        this.bottom.innerHTML = "";
+
+        var lowest = this.chartClass.getLowestNodeValue(CGJSNodeValues.actualSecondValue);
+        var highest = this.chartClass.getHighestNodeValue(CGJSNodeValues.actualSecondValue);
+
+        const lowestTimeElement = document.createElement("span");
+        lowestTimeElement.textContent = lowest;
+        lowestTimeElement.classList.add("cgjs-horizontal-value");
+        this.bottom.appendChild(lowestTimeElement);
+
+        //var timeSplit = parseInt(this.chartClass.getAttribute("time-split"));
+        //var timeNormal = (highestTime-lowestTime) / (timeSplit+1);
+        //var lastValue = lowestTime;
+        //console.log(timeNormal);
+        //for(var i=1;i <= timeSplit;i++) {
+        //    const timeElement = document.createElement("span");
+        //    lastValue = lastValue + 1*timeNormal;
+        //    timeElement.textContent = lastValue;
+        //    
+        //    timeElement.classList.add("cgjs-horizontal-value");
+        //    this.bottom.appendChild(timeElement);
+        //}
+
+        const highestTimeElement = document.createElement("span");
+        highestTimeElement.textContent = highest;
+        highestTimeElement.classList.add("cgjs-horizontal-value");
+        this.bottom.appendChild(highestTimeElement);
+    }
+
+    formatFirstValueToReadable(value) {
+        return "$"+value.toFixed(2);
+    }
+
+    firstValueToValidNumber(value) {
+        return parseFloat(value);
+    }
+
+    clearChart() {
+        this.chartCanvas.getContext("2d").clearRect(0, 0, this.chartCanvas.width, this.chartCanvas.height);
+        this.chartCanvasDashed.getContext("2d").clearRect(0, 0, this.chartCanvasDashed.width, this.chartCanvasDashed.height);
+        this.chartCanvasPoints.getContext("2d").clearRect(0, 0, this.chartCanvasPoints.width, this.chartCanvasPoints.height);
+        this.nodePointArray = []
+    }
+
+    drawLine(nodeId, x, y, x2, y2, dashed, isFirst) {
+        //console.log(nodeId, x,y, x2, y2);
+        // Fix values
+        if (x == undefined || y == undefined || x2 == undefined || y2 == undefined) {return;}
+
+        // Fix Canvas size
+        if (!this.chartCanvas.width) {
+            this.chartCanvas.width = this.chartCanvas.clientWidth;
+            this.chartCanvasDashed.width = this.chartCanvas.clientWidth;
+        }
+        if (!this.chartCanvas.height) {
+            this.chartCanvas.height = this.chartCanvas.clientHeight;
+            this.chartCanvasDashed.height = this.chartCanvas.clientHeight;
+        }
+
+        var width = this.chartCanvas.width;
+        var height = this.chartCanvas.height;
+
+        if (width == undefined || height == undefined) {return;}
+
+        //console.log("Line", x2, y2);
+
+        // Draw line
+        var ctx;
+
+        if (dashed) {
+            ctx = this.chartCanvasDashed.getContext("2d");
+            ctx.setLineDash([5, 2]);
+            ctx.lineWidth = 0.9;
+        } else {
+            ctx = this.chartCanvas.getContext("2d");
+            ctx.lineWidth = 0.7;
+
+            // Save point
+            ctx = this.chartCanvas.getContext("2d");
+            ctx.lineWidth = 0.7;
+            //console.log(nodeId, isFirst, x,y);
+            if (nodeId != undefined || !isFirst) {
+                this.nodePointArray.push({
+                    id: nodeId,
+                    x: x2*width,
+                    y: y2*height,
+                })
+            } else if (nodeId != undefined || isFirst) {
+                this.nodePointArray.push({
+                    id: nodeId,
+                    x: x*width,
+                    y: y*height,
+                })
+                return;
+            }
+        }
+
+        if (isFirst) {return;}
+
+        ctx.beginPath();
+        ctx.moveTo(x*width, y*height);
+        ctx.lineTo(x2*width, y2*height);
+        //0 182.40875912408757 976 182.40875912408757 true
+        //console.log(x*width, y*height, x2*width, y2*height, dashed);
+        ctx.strokeStyle = "black";
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+
+        let theme = this.chartClass.getAttribute("theme");
+        if (dashed) {
+            ctx.strokeStyle = CGJSThemes[theme].dashedLineColor;
+        } else {
+            ctx.strokeStyle = CGJSThemes[theme].lineColor;
+        }
+        
+        ctx.stroke();
+
+        //console.log(dashed);
+    }
+
+    drawNodePoint(x, y) {
+        // Fix Canvas size
+        if (!this.chartCanvasPoints.width) {
+            this.chartCanvasPoints.width = this.chartCanvasPoints.clientWidth;
+        }
+        if (!this.chartCanvasPoints.height) {
+            this.chartCanvasPoints.height = this.chartCanvasPoints.clientHeight;
+        }
+
+        var ctx = this.chartCanvasPoints.getContext("2d");
+        ctx.beginPath();
+
+        var width = this.chartCanvasPoints.width;
+        var height = this.chartCanvasPoints.height;
+
+        //console.log(x,y);
+        var offset = 7;
+        ctx.arc(x+offset, y+offset, 4, 0, 2 * Math.PI);
+
+        let theme = this.chartClass.getAttribute("theme");
+        ctx.fillStyle = CGJSThemes[theme].lineColor;
+
+        ctx.fill()
+    }
+
+    searchNearestNode(x1, y1) {
+        //console.log(x, y);
+        //console.log(this.nodePointArray);
+        var closestMagnitude;
+        var closestNodeId;
+        this.nodePointArray.forEach(point=>{
+            var x2 = point.x;
+            var y2 = point.y;
+            var magnitude = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+            //console.log(point);
+            if (!closestMagnitude) {closestMagnitude = magnitude; closestNodeId = point.id}
+            //if (point.id) {console.log(point.id, magnitude, closestMagnitude)}
+            if (magnitude < closestMagnitude) {
+                closestMagnitude = magnitude;
+                closestNodeId = point.id;
+            }
+        })
+        //console.log("Closest:", closestNodeId);
+        return closestNodeId;
+    }
+
+    showNodeLabel(values, node) {
+        //console.log(values, node)
+        this.chartCanvasPoints.getContext("2d").clearRect(0, 0, this.chartCanvasPoints.width, this.chartCanvasPoints.height); // Clear all other points
+        if (!this.label) {
+            this.label = document.createElement("div");
+            this.label.classList.add("cgjs-chart-label");
+            this.chartClass.shadowRoot.appendChild(this.label);        
+            //this.labelValueElements = {};
+        } else {
+            this.label.innerHTML = "";
+        }
+
+        values.forEach((valueName)=>{
+            const valueDiv = document.createElement("div");
+            valueDiv.classList.add("cgjs-chart-label-value");
+
+            const valueNameElement = document.createElement("span");
+            valueNameElement.textContent = valueName;
+
+            const valueElement = document.createElement("span");
+
+            if (this.chartClass.getAttribute(CGJSNodeValues.firstValue) == valueName) {
+                valueElement.textContent = this.formatFirstValueToReadable(parseFloat(node[CGJSNodeValues.actualFirstValue]));
+            }
+            if (this.chartClass.getAttribute(CGJSNodeValues.secondValue) == valueName) {
+                valueElement.textContent = this.formatSecondValueToReadable(parseInt(node[CGJSNodeValues.actualSecondValue]));
+            }
+
+            valueDiv.appendChild(valueNameElement);
+            valueDiv.appendChild(valueElement);
+            this.label.appendChild(valueDiv);
+
+            //this.labelValueElements[valueName] = valueElement;
+        })
+
+        var nodePoint = this.getNodePointById(node.id);
+        //console.log(nodePoint);
+        var offsetLeft = this.label.clientWidth/1.6;
+        var offsetTop = this.label.clientHeight/4;
+        var totalLeft = nodePoint.x-offsetLeft;
+        var totalTop = nodePoint.y-offsetTop;
+
+        if (totalLeft < this.label.clientWidth) {
+            //console.log("Too left");
+            totalLeft = nodePoint.x+offsetLeft/1.6;
+        }
+        if (totalTop < this.label.clientHeight) {
+            //console.log("Too left");
+            totalTop = nodePoint.y+offsetTop*4;
+        }
+
+        this.label.style.left = totalLeft+"px";
+        this.label.style.top = totalTop+"px";
+        this.label.style.visibility = "visible";
+
+        this.drawNodePoint(nodePoint.x, nodePoint.y);
+    }
+
+    hideNodeLabel() {
+        //console.log(this.label);
+        if (this.label) {this.label.style.visibility = "hidden";}
+        this.chartCanvasPoints.getContext("2d").clearRect(0, 0, this.chartCanvasPoints.width, this.chartCanvasPoints.height); // Clear all points
+    }
+
+    // FUNCTIONS
     getNodePointById(nodeId) {
         var node;
         this.nodePointArray.forEach(nodePoint=>{
